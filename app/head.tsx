dfea4134bcb29301c261d93219e597d7f1385285
Zaryab Ghori <imzaryab@gmail.com>
2023-03-28 04:01:14 +0500

    Changed Title to Linktree Portfolio

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
     return (
       <>
-        <title>Linktree Clone</title>
+        <title>Linktree Portfolio</title>
         <meta name="description" content="Next.js + Tailwind CSS" />
         <link rel="icon" href="/favicon.ico"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
